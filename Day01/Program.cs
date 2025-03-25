ea5a7f7f577866d92e55f389b8ccd69c5f71d4ca
priyankarangole02 <priyanka.rangole@sunbeaminfo.com>
2025-03-25 08:18:38 +0530

    console aplication created and entery point and Console.WriteLine and Compilation process

---
+++ Day01/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//C# most popular lang,free,cross platform,ron n everywhere,used to write phone ,computers ,servers,also most of the
+//dotnet rumtiome and libraries written in c#
+//C# is C family language C# syntax is familar to c,c++,java
+//end of statement;
+//Strongly typed language,type known as compile time
+
+namespace HelloCSharp
+{
+
+    //entery point of program
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteLine method comes from Console class which in System namespace
+            //System namespace was implicitly included
+            Console.WriteLine();
+        }
+    }
+}
